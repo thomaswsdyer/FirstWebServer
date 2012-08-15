b24d0f8cba56b4339befcde50fba4738c7bb7822
tomdyer <tom.dyer@gmail.com>
2012-08-15 10:56:41 -0600

    Adjusting description on home page

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,7 +5,7 @@ var querystring = require("querystring"),
 function home(response) {
 	console.log("Request handler for 'home' was called.\n");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Local node.js Web Server for testing.\n\n\tThis server provides the framework for adding ad-hoc Request Handlers for testing purposes.\n\n\tPlease review the README for more information. https://github.com/thomaswsdyer/FirstWebServer");
+	response.write("Local node.js Web Server for testing.\n\n\tThis server provides the framework for adding ad-hoc Request Handlers for testing purposes.\n\n\tPlease review the README for more information. https://github.com/thomaswsdyer/FirstWebServer\n\n\n\tCurrent Handlers:\n\t\t/\n\t\t/home");
   //response.write("Tom Dyer's Website for playing around with node.js\n\n\tTo Upload an image file, go to http://tomdyer.ca/start\n\n\n\n\n\n\t\t\tMerry Xmas Everyone!");
 	response.end();
 }
